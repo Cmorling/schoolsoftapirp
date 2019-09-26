9376dceb5dc2e5eec24139a72a90d85a342fb437
Caspian Mörling <caspianmorling@Caspians-MacBook-Air.local>
2019-09-26 21:33:22 +0200

    First commit

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from src import main
+
+app = Flask(__name__)
+student=main.schoolsoft("caspian.morling", "C4sp14nM0rl1ng_schoolsoft")
+student.login()
+@app.route('/food')
+def hello_world():
+    menu=student.get_food()
+    menuString = "The food for today is: "
+    for i in menu:
+        menuString = menuString + i + ", "
+    menuString = menuString[:-2]
+    return menuString
